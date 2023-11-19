ca1dcd092312c3df3fcc8d47e38d88bcdc540da9
Ewa <zalewskaewa7@wp.pl>
2023-11-19 23:51:51 +0100

    finish footer, finish desktop page

--- src/components/footer/footerMenu.js
+++ src/components/footer/footerMenu.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import "./scss/footerMenu.scss"
 
 function footerMenu() {
   return (
-    <div>footerMenu</div>
+    <ol>
+      <li>  Our company
+        <ul>
+        <li>How we work</li>
+        <li>Why Insure?</li>
+        <li>View plans</li>
+        <li>Reviews</li>
+        </ul>
+      </li>
+      <li>Help me
+      <ul>
+        <li>FAQ</li>
+        <li>Terms of use</li>
+        <li>Privacy policy</li>
+        <li>Cookies</li>
+      </ul>
+      </li>
+      <li>Contact
+      <ul>
+        <li>Sales</li>
+        <li>Support</li>
+        <li>Live chat</li>
+        </ul>
+      </li>
+      <li>Others
+      <ul>
+        <li>Careers</li>
+        <li>Press</li>
+        <li>Licenses</li>
+        </ul>
+      </li>
+
+    </ol>
   )
 }
 
--- src/components/header/navigation.js
+++ src/components/header/navigation.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import "./scss/navigation.scss";
+import hamburgerIcon from "../images/icon-hamburger.svg"
 
 function navigation() {
   return (
     <nav>
+        <img src={hamburgerIcon} alt={"hamburger icon"}/>
         <ul>
             <li>How we work</li>
             <li>Blog</li>
             <li>Account</li>
             <li className="liViewPlans">View plans</li>
         </ul>
+
     </nav>
   )
 }
